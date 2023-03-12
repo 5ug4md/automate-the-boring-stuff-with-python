f538dc5c8a19b86a161ee048db7ea6ef55321374
5ug4m <2077csit31@hcoe.edu.np>
2023-03-12 09:08:13 +0545

    lelxd

---
+++ chapter1/solution.py
@@ -0,0 +1,59 @@
+#1. Which of the following are operators, and which are values?
+# *
+# 'hello'
+# -88.8
+# -
+# /
+# +
+# 5
+
+#* is a operator for multiplication
+#'hello' is a string
+#-88.8 is a float
+#- is a operator for subtraction
+#/ is a operator for division
+#+ is a operator for addition
+#5 is a integer
+
+#2. Which of the following is a variable, and which is a string?
+# spam
+# 'spam'
+#spam is variable and 'spam' is a string
+
+#3. Name three data types.
+#integer
+#float
+#string
+
+# 4. What is an expression made up of? What do all expressions do?
+#An expression is made up of values and operators. All expressions evaluate down to a single value.
+
+#5. This chapter introduced assignment statements, like spam = 10. What is the difference between an expression and a statement?
+#An expression evaluates to a single value. A statement does not.
+
+
+#6. What does the variable bacon contain after the following code runs?
+#bacon = 20
+#bacon + 1
+
+#7. What should the following two expressions evaluate to?
+#'spam' + 'spamspam'
+# 'spam' * 3
+
+# 'spam' + 'spamspam' = 'spamspamspam'
+# 'spam' * 3 = 'spamspamspam'
+
+
+#8. Why is eggs a valid variable name while 100 is invalid?
+#A variable name cannot begin with a number.
+
+#9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+#int() , float() , str()
+
+#10. Why does this expression cause an error? How can you fix it?
+#'I have eaten ' + 99 + ' burritos.'
+#The expression causes an error because you cannot concatenate a string and an integer. You can fix it by converting the integer to a string with the str() function.
+
+
+
+
---
+++ list and tuples/practiceprojects/commacode.py
@@ -0,0 +1,7 @@
+import sys
+def commacode(LisT):
+    if len(list) == 0:
+        print("enter a non empty list")
+        sys.exit()
+    
+    
